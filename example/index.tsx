efea4597c0cdef21750bd3267eafe576f1f371cb
beepsaidblue <beepsaidblue@protonmail.com>
2022-03-06 15:01:00 +0800

    Add scaffolding methods

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,15 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { OrderList, Sell } from '../.';
+import { OrderList, OrderDetail, Sell } from '../.';
 
 const App = () => {
   return (
     <div>
       <h1>Order List</h1>
       <OrderList />
+      <h1>Order Detail</h1>
+      <OrderDetail />
       <h1>Sell</h1>
       <Sell />
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,71 @@
 import * as React from 'react';
 
+/**
+ * Core Candy Shop module
+ */
+export class CandyShop {
+  private _storeId: string;
+
+  constructor(storeId: string) {
+    this._storeId = storeId;
+  }
+
+  // TODO
+  async getOrders() {
+    return this._storeId;
+  }
+
+  // TODO
+  async buy() {
+
+  }
+
+  // TODO
+  async sell() {
+
+  }
+
+  // TODO
+  async cancel() {
+
+  }
+
+  // TODO
+  async getStats() {
+
+  }
+
+  // TODO
+  async getTransactions() {
+
+  }
+}
+
+/**
+ * React component for user to buy an NFT
+ */
+export const Buy = () => {
+  return (
+    <div>
+      <button>Buy</button>
+    </div>
+  );
+}
+
+/**
+ * React component for user to sell an NFT
+ */
+export const Sell = () => {
+  return (
+    <div>
+      <button>Sell</button>
+    </div>
+  );
+}
+
+/**
+ * React component that displays a list of orders
+ */
 export const OrderList = () => {
   return (
     <div style={{ maxWidth: 1600, margin: '0 auto'}}>
@@ -40,10 +106,14 @@ export const OrderList = () => {
   );
 };
 
-export const Sell = () => {
+/**
+ * React component that displays a single sell order
+ */
+export const OrderDetail = () => {
   return (
-    <div>
-      <button>Sell</button>
+    <div style={{ maxWidth: 1600, margin: '0 auto'}}>
+      OrderDetail
+      <Buy />
     </div>
   );
 }
