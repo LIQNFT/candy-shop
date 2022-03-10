d0b7bf47d9775b1b5399e31dea4002ec7c8b3f24
khacy <y.le@spiritlabs.co>
2022-03-10 09:42:42 +0700

    feat: skeleton for order list

--- src/components/OrderList/index.tsx
+++ src/components/OrderList/index.tsx
@@ -1,7 +1,7 @@
 /**
  * React component that displays a list of orders
  */
-import { Card, Col, Row } from 'antd';
+import { Card, Col, Row, Skeleton } from 'antd';
 import React, { useCallback, useEffect, useState } from 'react';
 import { fetchOrderByStoreId } from '../../api/backend/OrderAPI';
 import BuyModal from '../BuyModal';
@@ -11,6 +11,7 @@ import './style.less';
 const OrderList = () => {
   const [orderList, setOrderList] = useState<any[]>([]);
   const [order, setOrder] = useState();
+  const [loading, setLoading] = useState(false);
 
   const onClick = useCallback(
     (idx: number) => () => {
@@ -25,9 +26,11 @@ const OrderList = () => {
 
   useEffect(() => {
     (async () => {
+      setLoading(true);
       // Fetch order list
       fetchOrderByStoreId('BZHgtcQ47QJg7WnAF73sxRtH5vQT2DFUMTowEhqiL4ks')
         .then((data: any) => {
+          setLoading(false);
           setOrderList(data.result);
         })
         .catch(err => {
@@ -39,32 +42,41 @@ const OrderList = () => {
   return (
     <div className="order-list">
       <Row gutter={50}>
-        {orderList.map((item, key) => (
-          <Col key={key} md={8}>
-            <Card
-              className="order-item"
-              onClick={onClick(key)}
-              cover={
-                <div className="order-thumbnail">
-                  <img
-                    src={
-                      item?.nftImageLink || 'https://via.placeholder.com/300'
-                    }
-                  />
-                </div>
-              }
-            >
-              <div>
-                <p className="candy-label">ARTIST_NAME</p>
-                <p>{item.name}</p>
-              </div>
-              <div>
-                <p className="candy-label">PRICE</p>
-                <p>{(+item.price / 10e9).toFixed(3)} SOL</p>
-              </div>
-            </Card>
-          </Col>
-        ))}
+        {loading
+          ? Array(6)
+              .fill(0)
+              .map((_, key) => (
+                <Col key={key} md={8}>
+                  <Skeleton />
+                </Col>
+              ))
+          : orderList.map((item, key) => (
+              <Col key={key} md={8}>
+                <Card
+                  className="order-item"
+                  onClick={onClick(key)}
+                  cover={
+                    <div className="order-thumbnail">
+                      <img
+                        src={
+                          item?.nftImageLink ||
+                          'https://via.placeholder.com/300'
+                        }
+                      />
+                    </div>
+                  }
+                >
+                  <div>
+                    <p className="candy-label">ARTIST_NAME</p>
+                    <p>{item.name}</p>
+                  </div>
+                  <div>
+                    <p className="candy-label">PRICE</p>
+                    <p>{(+item.price / 10e9).toFixed(3)} SOL</p>
+                  </div>
+                </Card>
+              </Col>
+            ))}
       </Row>
 
       {order && (
