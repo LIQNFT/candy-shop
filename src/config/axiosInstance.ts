f9706a031918f8489f9b9762cb1e837c7ba79158
RooTheRu <crypto.bortfolio@gmail.com>
2022-03-15 10:34:24 +0800

    Update staging candyShop url

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -5,7 +5,7 @@ const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  baseURL: `http://ec2-52-201-255-145.compute-1.amazonaws.com/api`,
+  baseURL: `https://ckaho.liqnft.com/api`,
 });
 
 export default axiosInstance;
