3d11a00db6840d1b3ac4e9b9e412b150574aad07
SoloVerse <jokeygeek@gmail.com>
2022-04-15 23:36:00 +0800

    [chore] Update readme and add documentation ts comment
    - Rename `Settings` to `CandyShopSettings`, and export it.
    - CandyShop class minor refactoring

--- lib/src/index.tsx
+++ lib/src/index.tsx
@@ -1,6 +1,6 @@
 import './index.less';
 
-export { CandyShop } from './core/CandyShop';
+export { CandyShop, CandyShopSettings } from './core/CandyShop';
 export { Orders } from './core/Orders';
 export { OrderDetail } from './core/OrderDetail';
 export { Sell } from './core/Sell';
