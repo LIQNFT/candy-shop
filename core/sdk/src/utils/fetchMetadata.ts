7ff864ee21e83d79fa9a1e2fd9be9d3a27b87ed3
SoloVerse <jokeygeek@gmail.com>
2022-05-01 14:22:39 +0800

    [core] Progressive loading user NFTs with batch to render UI
    [LIQ-470]
    
    - Refactor the singleTokenInfoPromise call in core/sdk.
    
    - Refactor the fetchNftsFromWallet API to provide the ability for
      getting the batch result once NFT batch is loaded by callback.
    
    - Dynamically choose the ms interval by provided batch size but
      restricted with default upper bound to play safe with current
      endpoint.
    
    - Clean up codes in Sell/index.tsx in core/ui and let it utilize
      batch result from fetchNftsFromWallet to render NFT cards.

--- core/sdk/src/utils/fetchMetadata.ts
+++ core/sdk/src/utils/fetchMetadata.ts
@@ -1,14 +1,20 @@
 import { web3 } from '@project-serum/anchor';
-import { getAccount } from '@solana/spl-token';
+import { Account, getAccount } from '@solana/spl-token';
 import axios from 'axios';
 import * as crc32 from 'crc-32';
 import { safeAwait } from './promiseUtils';
 import { Metadata, parseEdition, parseMetadata } from './parseData';
 
 const METADATA_PROGRAM_ID = 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s';
-const metadataProgramId = new web3.PublicKey(METADATA_PROGRAM_ID);
+const MetadataProgramPubkey = new web3.PublicKey(METADATA_PROGRAM_ID);
 
-export type SingleTokenInfo = {
+interface NFTMetadataInfo {
+  nftImage: string;
+  nftAnimation: string | undefined;
+  nftDescription: string;
+}
+
+export interface SingleTokenInfo {
   tokenAccountAddress: string;
   metadata: Metadata | undefined;
   edition: string | undefined;
@@ -16,103 +22,141 @@ export type SingleTokenInfo = {
   nftImage: string;
   nftAnimation: string | undefined;
   nftDescription: string;
-};
+}
+
+// TODO: we will need extendable singleTokenInfoPromise that can apply different param
+export interface SingleTokenInfoPromiseParam {
+  connection: web3.Connection;
+  tokenAccountAddress: string;
+  identifiers: string[];
+}
 
 export const singleTokenInfoPromise = async (
-  connection: web3.Connection,
-  tokenAccountAddress: string,
-  identifiers: string[]
+  param: SingleTokenInfoPromiseParam
 ): Promise<SingleTokenInfo | null> => {
-  // Get account
-  const token = await getAccount(
+  const connection = param.connection;
+  const tokenAccountAddress = param.tokenAccountAddress;
+  const identifiers = param.identifiers;
+
+  const tokenAccount = await getAccount(
     connection,
     new web3.PublicKey(tokenAccountAddress)
   );
-  const [newEditionMetadata] = await web3.PublicKey.findProgramAddress(
-    [
-      Buffer.from('metadata'),
-      metadataProgramId.toBuffer(),
-      token.mint.toBuffer()
-    ],
-    metadataProgramId
-  );
-  const [newEditionPublicKey] = await web3.PublicKey.findProgramAddress(
-    [
-      Buffer.from('metadata'),
-      metadataProgramId.toBuffer(),
-      token.mint.toBuffer(),
-      Buffer.from('edition')
-    ],
-    metadataProgramId
-  );
-
-  const newEditionMetadataAccountInfoResult = await safeAwait(
-    connection.getAccountInfo(newEditionMetadata)
-  );
-  if (newEditionMetadataAccountInfoResult.error) {
-    console.log('rate limited');
-  }
-  const newEditionMetadataAccountInfo =
-    newEditionMetadataAccountInfoResult.result;
-
-  const newEditionAccountInfoResult = await safeAwait(
-    connection.getAccountInfo(newEditionPublicKey)
-  );
-  if (newEditionAccountInfoResult.error) {
-    console.log('rate limited');
-  }
-  const newEditionAccountInfo = newEditionAccountInfoResult.result;
-
-  const tokenInfo = newEditionMetadataAccountInfo
-    ? parseMetadata(newEditionMetadataAccountInfo.data)
-    : undefined;
-  const tokenEdition = newEditionAccountInfo
-    ? parseEdition(newEditionAccountInfo?.data).edition.toString()
-    : undefined;
-
-  if (tokenInfo) {
-    // collection check here
-    if (identifiers.length > 0 && !isValidCollection(identifiers, tokenInfo))
-      return null;
-
-    return axios
-      .get(tokenInfo.data.uri)
-      .then((res) => {
-        const nftUriData = res.data as any;
-        const nftImage = nftUriData.image;
-        const nftAnimation: string | undefined = nftUriData.animation_url;
-        const nftDescription = nftUriData.description;
-
-        return {
-          tokenAccountAddress: tokenAccountAddress,
-          metadata: tokenInfo,
-          edition: tokenEdition,
-          tokenMintAddress: token.mint.toString(),
-          nftImage,
-          nftAnimation,
-          nftDescription
-        };
-      })
-      .catch((err) => {
-        console.log('failed to fetch uri data');
-        console.log(err);
-        return null;
-      });
-  } else {
-    console.log(`tokenAccount ${tokenAccountAddress} does not have metadata`);
+  const tokenInfo = await getNFTMetadataAccountInfo(connection, tokenAccount);
+  if (!tokenInfo) {
+    console.log(
+      `singleTokenInfoPromise: tokenAccount ${tokenAccountAddress} does not have metadata`
+    );
     return null;
   }
+
+  if (identifiers.length > 0 && !isValidCollection(identifiers, tokenInfo)) {
+    return null;
+  }
+  const nftMetaDataInfo = await fetchMetadataInfoByUri(tokenInfo.data.uri);
+
+  if (!nftMetaDataInfo) {
+    return null;
+  }
+  const tokenEdition = await getNFTEditionInfo(connection, tokenAccount);
+  const singleTokenInfo: SingleTokenInfo = {
+    tokenAccountAddress: tokenAccountAddress,
+    metadata: tokenInfo,
+    edition: tokenEdition,
+    tokenMintAddress: tokenAccount.mint.toString(),
+    nftImage: nftMetaDataInfo.nftImage,
+    nftAnimation: nftMetaDataInfo.nftAnimation,
+    nftDescription: nftMetaDataInfo.nftDescription
+  };
+  return singleTokenInfo;
 };
 
-function isValidCollection(identifiers: string[], tokenInfo: Metadata) {
+const getNFTMetadataAccountInfo = async (
+  connection: web3.Connection,
+  tokenAccount: Account
+): Promise<Metadata | undefined> => {
+  const [nftMetadataPublicKey] = await web3.PublicKey.findProgramAddress(
+    [
+      Buffer.from('metadata'),
+      MetadataProgramPubkey.toBuffer(),
+      tokenAccount.mint.toBuffer()
+    ],
+    MetadataProgramPubkey
+  );
+  const nftMetadataAccountInfo = await safeAwait(
+    connection.getAccountInfo(nftMetadataPublicKey)
+  );
+  if (nftMetadataAccountInfo.error) {
+    // TODO: handle the error correctly
+    console.log('rate limited');
+  }
+
+  const tokenInfo = nftMetadataAccountInfo.result
+    ? parseMetadata(nftMetadataAccountInfo.result.data)
+    : undefined;
+  return tokenInfo;
+};
+
+const getNFTEditionInfo = async (
+  connection: web3.Connection,
+  tokenAccount: Account
+): Promise<any> => {
+  const [nftEditionPublicKey] = await web3.PublicKey.findProgramAddress(
+    [
+      Buffer.from('metadata'),
+      MetadataProgramPubkey.toBuffer(),
+      tokenAccount.mint.toBuffer(),
+      Buffer.from('edition')
+    ],
+    MetadataProgramPubkey
+  );
+
+  const nftEditionAccountInfo = await safeAwait(
+    connection.getAccountInfo(nftEditionPublicKey)
+  );
+  if (nftEditionAccountInfo.error) {
+    // TODO: handle the error correctly
+    console.log('rate limited');
+  }
+
+  const editionInfo = nftEditionAccountInfo.result
+    ? parseEdition(nftEditionAccountInfo.result.data).edition.toString()
+    : undefined;
+  return editionInfo;
+};
+
+const fetchMetadataInfoByUri = async (
+  nftUri: string
+): Promise<NFTMetadataInfo | null> => {
+  const res = await safeAwait(axios.get(nftUri));
+  if (res.error) {
+    console.log(
+      'singleTokenInfoPromise: Failed to fetch uri data, error=',
+      res.error
+    );
+    return null;
+  }
+  const nftUriData = res.result.data as any;
+  const nftImage = nftUriData.image;
+  const nftAnimation: string | undefined = nftUriData.animation_url;
+  const nftDescription = nftUriData.description;
+
+  const nftMetaDataInfo: NFTMetadataInfo = {
+    nftImage: nftImage,
+    nftAnimation: nftAnimation,
+    nftDescription: nftDescription
+  };
+  return nftMetaDataInfo;
+};
+
+const isValidCollection = (identifiers: string[], tokenInfo: Metadata) => {
   const creators = tokenInfo.data.creators?.map((creator) => ({
     address: new web3.PublicKey(creator.address).toString(),
     verified: creator.verified,
     share: creator.share
   }));
   const symbol = tokenInfo.data.symbol;
-
   const str = symbol + JSON.stringify(creators);
   const identifier = crc32.str(str).toString();
   return identifiers.includes(identifier);
-}
+};
