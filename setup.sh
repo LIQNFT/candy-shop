135141444b96dae0e944f187a9362d0cad64f8f1
SoloVerse(Wen) <11007204+haVincy@users.noreply.github.com>
2022-05-10 10:12:08 +0800

    [chore] Add --link-build in setup.sh to do symbolic link optionally (#154)
    We're rapidly updating sdk along with ui, most of time we need to
    link local packages, combine the link process in setup.sh but as
    optional to provide flexibility when we want to check clean build.
    
    - Removed the workspace_symbolic_link.sh.

--- setup.sh
+++ setup.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
-
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # install pkgs in root
 yarn
 # install and build pkgs in core/sdk
 cd core/sdk
 yarn
 yarn build
+# if first argument has --link-build, using symbolic link
+if [[ -n $1 && $1 == "--link-build" ]]
+then
+    echo -e "\033[33mCreate link in core/sdk\033[m"
+    yarn link
+fi
 
 cd $SCRIPTPATH
 # install and build pkgs in core/ui
 cd core/ui
 yarn
+
+if [[ -n $1 && $1 == "--link-build" ]]
+then
+    echo -e "\033[33mLink core/ui modules to core/sdk\033[m"
+    yarn link @liqnft/candy-shop-sdk
+fi
 yarn build
 
 # build pkgs for example
