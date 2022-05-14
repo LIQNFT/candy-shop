2ab146d970684716c4b353aaaa919b88ad92fc5a
beepsaidblue <beepsaidblue@protonmail.com>
2022-05-14 09:32:33 +0800

    [core/cli] update and add sell many function

--- core/cli/src/helper/account.ts
+++ core/cli/src/helper/account.ts
@@ -1,4 +1,5 @@
 import { web3 } from '@project-serum/anchor';
+import { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import fs from 'fs';
 
 export const CANDY_SHOP_PROGRAM_ID = new web3.PublicKey('csa8JpYfKSZajP7JzxnJipUL3qagub1z29hLvp578iN');
@@ -8,4 +9,20 @@ export const CANDY_SHOP_INS_PROGRAM_ID = new web3.PublicKey('cseLFQDeAfo96uQhA6M
 export function loadKey(keypair: string): web3.Keypair {
   const wallet = web3.Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync(keypair).toString())));
   return wallet;
+}
+
+export function loadTokenAccountMints(path: string): string[] {
+  return JSON.parse(fs.readFileSync(path).toString());
+}
+
+export async function findAssociatedTokenAddress(
+  walletAddress: web3.PublicKey,
+  tokenMintAddress: web3.PublicKey
+): Promise<web3.PublicKey> {
+  return (
+    await web3.PublicKey.findProgramAddress(
+      [walletAddress.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), tokenMintAddress.toBuffer()],
+      ASSOCIATED_TOKEN_PROGRAM_ID
+    )
+  )[0];
 }
