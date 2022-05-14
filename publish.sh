a99a41d40b581902d4883b20cedd8398b71ee05a
beepsaidblue <beepsaidblue@protonmail.com>
2022-05-14 14:23:43 +0800

    [chore] publish script

---
+++ publish.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [[ "$*" == *"sdk"* || "$*" == *"ui"* ]]
+then
+    echo -e "\033[33mBump versions\033[m"
+    ts-node bump.ts $@
+fi
+
+if [[ "$*" == *"sdk"* ]]
+then
+    echo -e "\033[33mClean build core/sdk\033[m"
+    cd core/sdk
+    rm -rf node_modules
+    yarn
+    yarn build
+
+    echo -e "\033[33mPublish core/sdk\033[m"
+    npm publish
+fi
+
+if [[ "$*" == *"ui"* ]]
+then
+    echo -e "\033[33mClean build core/ui\033[m"
+    cd $SCRIPTPATH
+    cd core/ui
+    rm -rf node_modules
+    yarn unlink @liqnft/candy-shop-sdk
+    yarn
+    yarn build
+
+    echo -e "\033[33mPublish core/ui\033[m"
+    npm publish
+fi
