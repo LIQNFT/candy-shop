f5498704d91bd95866de5840d0a1e247c7d4c38a
SoloVerse(Wen) <11007204+haVincy@users.noreply.github.com>
2022-05-18 12:54:13 +0800

    [ui] Refactor CandyShopDataValidator to use interval hook (#175)
    Replace the timeout with useInterval hook that provides cleaner
    codes and easier to maintain for polling.

---
+++ core/ui/src/hooks/useInterval.ts
@@ -0,0 +1,25 @@
+import { useEffect, useLayoutEffect, useRef } from 'react';
+
+// https://usehooks-ts.com/react-hook/use-interval
+// https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+export function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback);
+
+  // Remember the latest callback if it changes.
+  useLayoutEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    // Don't schedule if no delay is specified.
+    // Note: 0 is a valid value for delay.
+    if (!delay && delay !== 0) {
+      return;
+    }
+
+    const id = setInterval(() => savedCallback.current(), delay);
+
+    return () => clearInterval(id);
+  }, [delay]);
+}
