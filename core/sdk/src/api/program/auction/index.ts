ce506c5bbea87a0d758d86ce5c00b05f354d575b
Teddy <62431052+vuvu2259@users.noreply.github.com>
2022-05-19 16:37:24 +0700

    change makeBid to bid

--- core/sdk/src/api/program/auction/index.ts
+++ core/sdk/src/api/program/auction/index.ts
@@ -1,5 +1,4 @@
-// export * from './bid';
-// export * from './buyNow';
-export * from './cancel';
 export * from './create';
-export * from './makeBid';
+export * from './cancel';
+export * from './bid';
+// export * from './buyNow';
