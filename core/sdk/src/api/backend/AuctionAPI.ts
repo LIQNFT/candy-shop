36e682d3bcf2fbeed38ab33dcb00b2ab78091239
tvminh <van-minh.tran@spiritlabs.co>
2022-05-20 09:46:23 +0700

    wip: update AuctionAPI

--- core/sdk/src/api/backend/AuctionAPI.ts
+++ core/sdk/src/api/backend/AuctionAPI.ts
@@ -1,1 +1,29 @@
-// TODO
+import { SingleBase, ListBase, Auction, AuctionBid } from '@liqnft/candy-shop-types';
+import { AxiosInstance } from 'axios';
+
+export function fetchAuctionsByShop(axiosInstance: AxiosInstance, shopId: string): Promise<ListBase<Auction>> {
+  console.log('fetching Auctions By ShopId=', shopId);
+  return axiosInstance.get<ListBase<Auction>>(`/auction/${shopId}`).then((response) => response.data);
+}
+
+export function fetchAuctionBid(
+  axiosInstance: AxiosInstance,
+  auctionAddress: string,
+  walletAddress: string
+): Promise<SingleBase<AuctionBid>> {
+  console.log(`fetching Auction bid by auctionAddress=${auctionAddress}, walletAddress=${walletAddress}`);
+  return axiosInstance.get(`/auction/${auctionAddress}/wallet/${walletAddress}`).then((res) => res.data);
+}
+
+export function fetchAuction(axiosInstance: AxiosInstance, auctionAddress: string): Promise<SingleBase<Auction>> {
+  console.log(`fetching Auction detail, auctionAddress=${auctionAddress}`);
+  return axiosInstance.get(`/auction/${auctionAddress}`).then((res) => res.data);
+}
+
+export function fetchAuctionHistoryByAddress(
+  axiosInstance: AxiosInstance,
+  auctionAddress: string
+): Promise<ListBase<AuctionBid>> {
+  console.log(`fetching Auction bid history by auctionAddress=`, auctionAddress);
+  return axiosInstance.get(`/auction/history/${auctionAddress}`).then((res) => res.data);
+}
