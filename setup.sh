0e4f249c2f7463c8205e52fa4ce15c1f4bca1eff
SoloVerse(Wen) <11007204+haVincy@users.noreply.github.com>
2022-05-29 22:58:11 +0800

    [chore] Update CI to include core/types (#190)
    Reorg setup.sh a bit and update build.yml.

--- setup.sh
+++ setup.sh
@@ -3,38 +3,39 @@ SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # install pkgs in root
 yarn
+
 # install and build pkgs in core/types
 cd core/types
 yarn
-yarn build
 # if first argument has --link-build, using symbolic link
 if [[ -n $1 && $1 == "--link-build" ]]
 then
     echo -e "\033[33mCreate link in core/types\033[m"
     yarn link
 fi
+yarn build
 
 cd $SCRIPTPATH
 # install and build pkgs in core/sdk
 cd core/sdk
 yarn
-yarn build
 # if first argument has --link-build, using symbolic link
 if [[ -n $1 && $1 == "--link-build" ]]
 then
-    echo -e "\033[33mCreate link in core/sdk\033[m"
+    echo -e "\033[33mLink core/sdk to core/types\033[m"
     yarn link @liqnft/candy-shop-types
+    echo -e "\033[33mCreate link in core/sdk\033[m"
     yarn link
 fi
+yarn build
 
 cd $SCRIPTPATH
 # install and build pkgs in core/ui
 cd core/ui
 yarn
-
 if [[ -n $1 && $1 == "--link-build" ]]
 then
-    echo -e "\033[33mLink core/ui modules to core/sdk\033[m"
+    echo -e "\033[33mLink core/ui modules to core/sdk and core/types\033[m"
     yarn link @liqnft/candy-shop-types
     yarn link @liqnft/candy-shop-sdk
 fi
