7d0af66643bc13d194b50ebdcaf245517ec6d79f
tvminh <van-minh.tran@spiritlabs.co>
2022-05-30 16:16:25 +0700

    ui: update-Sell-component-status

--- core/ui/src/public/Context/index.tsx
+++ core/ui/src/public/Context/index.tsx
@@ -14,11 +14,13 @@ const Logger = 'CandyShopUI/CandyShopDataValidator';
 interface ContextData {
   candyShopAddress?: web3.PublicKey;
   setCandyShopAddress: Dispatch<SetStateAction<web3.PublicKey | undefined>>;
+  setWalletAddress: Dispatch<SetStateAction<string | undefined>>;
 }
 
 export const CandyContext = createContext<ContextData>({
   candyShopAddress: undefined,
-  setCandyShopAddress: () => console.log(`${Logger}: update CandyShopAddress`)
+  setCandyShopAddress: () => console.log(`${Logger}: update CandyShopAddress`),
+  setWalletAddress: () => console.log(`${Logger}: update User WalletAddress`)
 });
 
 interface CandyProviderProps {
@@ -27,28 +29,32 @@ interface CandyProviderProps {
 
 export const CandyShopDataValidator: React.FC<CandyProviderProps> = ({ children }) => {
   const [candyShopAddress, setCandyShopAddress] = useState<web3.PublicKey>();
+  const [walletAddress, setWalletAddress] = useState<string>();
 
-  const polling = useCallback((candyShopAddress: web3.PublicKey) => {
-    fetchShopStatusByShopAddress(candyShopAddress)
-      .then((res: SingleBase<ShopStatus[]>) => {
-        if (res.result) {
-          for (const shopStatus of res.result) {
-            const prevTimestamp = localStorage.getItem(shopStatus.type);
-            const resTimestamp = JSON.stringify(shopStatus.timestamp);
-            const isShopRefreshed = prevTimestamp !== resTimestamp;
-            if (isShopRefreshed) {
-              console.log(`${Logger}: ${shopStatus.type}`, prevTimestamp, res);
-              localStorage.setItem(shopStatus.type, JSON.stringify(shopStatus.timestamp));
+  const polling = useCallback(
+    (candyShopAddress: web3.PublicKey) => {
+      fetchShopStatusByShopAddress(candyShopAddress, walletAddress)
+        .then((res: SingleBase<ShopStatus[]>) => {
+          if (res.result) {
+            for (const shopStatus of res.result) {
+              const prevTimestamp = localStorage.getItem(shopStatus.type);
+              const resTimestamp = JSON.stringify(shopStatus.timestamp);
+              const isShopRefreshed = prevTimestamp !== resTimestamp;
+              if (isShopRefreshed) {
+                console.log(`%c ${Logger}: ${shopStatus.type}, prevTimestamp=${prevTimestamp}`, 'color: orange;', res);
+                localStorage.setItem(shopStatus.type, JSON.stringify(shopStatus.timestamp));
+              }
             }
+          } else {
+            console.log(`${Logger}: fetchShopStatus res.result is undefined`);
           }
-        } else {
-          console.log(`${Logger}: fetchShopStatus res.result is undefined`);
-        }
-      })
-      .catch((err: any) => {
-        console.log(`${Logger}: fetchShopStatus failed, error=`, err);
-      });
-  }, []);
+        })
+        .catch((err: any) => {
+          console.log(`${Logger}: fetchShopStatus failed, error=`, err);
+        });
+    },
+    [walletAddress]
+  );
 
   // polling update shop content
   useInterval(
@@ -59,22 +65,35 @@ export const CandyShopDataValidator: React.FC<CandyProviderProps> = ({ children 
     candyShopAddress ? POLLING_SHOP_INTERVAL : null
   );
 
-  return <CandyContext.Provider value={{ candyShopAddress, setCandyShopAddress }}>{children}</CandyContext.Provider>;
+  return (
+    <CandyContext.Provider value={{ candyShopAddress, setCandyShopAddress, setWalletAddress }}>
+      {children}
+    </CandyContext.Provider>
+  );
 };
 
-export const useUpdateCandyShopContext: (candyShopProp?: web3.PublicKey) => ContextData = (candyShopProp) => {
+export const useUpdateCandyShopContext: (candyShopProp?: web3.PublicKey, walletAddress?: string) => ContextData = (
+  candyShopProp,
+  walletAddress
+) => {
   const context = useContext(CandyContext);
   if (!context) {
     throw new Error('useUpdateCandyShopContext must be used within a CandyShopDataValidator');
   }
 
-  const { candyShopAddress, setCandyShopAddress } = context;
+  const { candyShopAddress, setCandyShopAddress, setWalletAddress } = context;
 
-  // update candyShopAddress in context store
+  // update candyShopAddress
   useEffect(() => {
     if (!candyShopProp) return;
     setCandyShopAddress(candyShopProp);
   }, [candyShopAddress, candyShopProp, setCandyShopAddress]);
+
+  // update walletAddress
+  useEffect(() => {
+    if (!walletAddress) return;
+    setWalletAddress(walletAddress);
+  }, [setWalletAddress, walletAddress]);
 
   return context;
 };
