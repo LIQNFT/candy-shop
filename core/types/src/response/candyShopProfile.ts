edbb39add658412c9a2600db4cff17ab2eb5a7ab
dong <nhnguhoc@gmail.com>
2022-06-27 12:01:46 +0700

    [core/types] add candyShopProfile response type

---
+++ core/types/src/response/candyShopProfile.ts
@@ -0,0 +1,12 @@
+export interface CandyShopProfile {
+  address: string;
+  name: string;
+  email: string;
+  description: string;
+  profilePic: string;
+  headerProfilePic: string;
+  instagramUrl: string;
+  twitterUrl: string;
+  updatedAt: Date;
+  createdAt: Date;
+}
