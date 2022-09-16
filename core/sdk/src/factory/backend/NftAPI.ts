f5944ebbc53fac67254d3bc75425520eff542baf
dongnguyenltqb <nhnguhoc@gmail.com>
2022-09-16 16:13:31 +0700

    feat/add list user eth wallet nft api

--- core/sdk/src/factory/backend/NftAPI.ts
+++ core/sdk/src/factory/backend/NftAPI.ts
@@ -1,7 +1,20 @@
-import { SingleBase, Nft } from '@liqnft/candy-shop-types';
+import { SingleBase, Nft, EthWalletNftQuery, ListBaseWithCurSor } from '@liqnft/candy-shop-types';
 import { AxiosInstance } from 'axios';
 
 export async function fetchNftByMint(axiosInstance: AxiosInstance, mint: string): Promise<Nft> {
   console.log('CandyShop: fetching NFT by mint address=', mint);
   return await axiosInstance.get<SingleBase<Nft>>(`/nft/${mint}`).then((response) => response.data.result);
+}
+
+export async function fetchUserEthWalletNft(
+  axiosInstance: AxiosInstance,
+  walletAddress: string,
+  query?: EthWalletNftQuery
+): Promise<ListBaseWithCurSor<Nft>> {
+  console.log(`CandyShop: fetchUserEthWalletNft by wallet address=${walletAddress} with query`, query);
+  return await axiosInstance
+    .get<ListBaseWithCurSor<Nft>>(`/nft/eth-wallet/:${walletAddress}`, {
+      params: query
+    })
+    .then((response) => response.data);
 }
--- core/sdk/src/index.ts
+++ core/sdk/src/index.ts
@@ -5,5 +5,6 @@ export * from './CandyShopPay';
 export * from './CandyShopInfoAPI';
 export * from './CandyShopAuctionAPI';
 export * from './CandyShopModel';
+export * from './CandyShopNftAPI';
 /* Only export the vendor folder to control the exposure of some internal methods */
 export * from './vendor';
