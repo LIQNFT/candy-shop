830ae9dd62e9d39890d07590385c20d805e6370b
RooTheRu <crypto.bortfolio@gmail.com>
2022-10-03 11:47:27 +0800

    [LIQ-833] Update edition drop mint condition check
    If the salesPeriod is 0, this means the sales will last
    forever until sell out.

--- core/sdk/src/vendor/utils/validationUtils.ts
+++ core/sdk/src/vendor/utils/validationUtils.ts
@@ -269,7 +269,10 @@ export const checkEditionMintPeriod = async (vaultAccount: PublicKey, program: P
   const currentTime: BN = new BN(Date.now() / 1000);
   const saleEndTime: BN = vaultData.startingTime.add(vaultData.salesPeriod);
 
-  if (currentTime.lt(vaultData.whitelistTime ?? vaultData.startingTime) || currentTime.gte(saleEndTime)) {
+  if (
+    currentTime.lt(vaultData.whitelistTime ?? vaultData.startingTime) ||
+    (currentTime.gte(saleEndTime) && !vaultData.salesPeriod.eq(new BN(0)))
+  ) {
     throw new CandyShopError(CandyShopErrorType.NotWithinSalesPeriod);
   }
   return vaultData;
