df14385b4bede737b144170d2b9d8959e0e34367
tvminh <van-minh.tran@spiritlabs.co>
2023-01-12 09:14:49 +0700

    [ui] fix sell modal price input UI

--- core/ui/src/utils/getPrice.ts
+++ core/ui/src/utils/getPrice.ts
@@ -1,4 +1,5 @@
 import { ShopExchangeInfo } from 'model';
+import { notification, NotificationType } from './rc-notification';
 
 export const getPrice = (
   shopPriceDecimalsMin: number,
@@ -12,4 +13,45 @@ export const getPrice = (
     minimumFractionDigits: shopPriceDecimalsMin,
     maximumFractionDigits: shopPriceDecimals
   });
+};
+
+export const getNumberFromPriceStr = (price: string) => {
+  const arr = price.split('.');
+
+  const replacement = arr[0].replace(/\d(?=(?:\d{3})+$)/g, '$&,');
+
+  if (price.includes('.') && arr[1] === '') {
+    return `${replacement}.`;
+  }
+
+  if (arr[1] === '0' || arr[1]) {
+    return replacement + `.${arr[1]}`;
+  }
+  return replacement;
+};
+
+export const enum SellPriceValidationState {
+  Valid = 'Valid',
+  Invalid = 'Invalid',
+  LessThanMinimum = 'LessThanMinimum'
+}
+
+export const validateSellPrice = (price: number, currencyDecimals: number): SellPriceValidationState => {
+  if (price * Math.pow(10, currencyDecimals) >= Number.MAX_SAFE_INTEGER) {
+    notification(
+      `The input sell price must less than ${Math.ceil(Number.MAX_SAFE_INTEGER / Math.pow(10, currencyDecimals))}`,
+      NotificationType.Error
+    );
+    return SellPriceValidationState.Invalid;
+  }
+  const minSellPrice = Math.pow(10, 2 - currencyDecimals);
+  if (isNaN(price) || price === 0) {
+    notification(`Please input valid sell price`, NotificationType.Error);
+    return SellPriceValidationState.Invalid;
+  }
+  if (price < minSellPrice) {
+    notification(`The input sell price must greater than or equal to ${minSellPrice}`, NotificationType.Error);
+    return SellPriceValidationState.LessThanMinimum;
+  }
+  return SellPriceValidationState.Valid;
 };
