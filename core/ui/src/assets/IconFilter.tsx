dca5eb85279ac039887917da1f648f84488344bb
tvminh <van-minh.tran@spiritlabs.co>
2023-02-21 15:46:05 +0700

    [ui] remove filter shop and add collection filter dropdown style

---
+++ core/ui/src/assets/IconFilter.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const IconFilter: React.FC = () => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      stroke-width="2"
+      viewBox="0 0 24 24"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      color="#f5f3f7"
+      height="24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon>
+    </svg>
+  );
+};
--- core/ui/src/components/CollectionFilter/index.tsx
+++ core/ui/src/components/CollectionFilter/index.tsx
@@ -1,23 +1,27 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { fetchAllCollection, fetchCollectionByShopAddress } from '@liqnft/candy-shop-sdk';
+import { fetchCollectionByShopAddress } from '@liqnft/candy-shop-sdk';
 import { ListBase, NftCollection } from '@liqnft/candy-shop-types';
 import { Processing } from 'components/Processing';
 import { CollectionFilter as CollectionFilterModel } from 'model';
 import { Search } from 'components/Search';
 import { removeDuplicate } from 'utils/helperFunc';
 import { LoadStatus } from 'constant';
-import '../../style/order-filter.less';
 import { handleError } from 'utils/ErrorHandler';
+import * as Popover from '@radix-ui/react-popover';
+import { IconFilter } from 'assets/IconFilter';
+import { Show } from 'components/Show';
+import '../../style/order-filter.less';
+
+export type FilterType = 'list' | 'dropdown';
 
 interface CollectionFilterProps {
   onChange: (item: NftCollection | CollectionFilterModel | undefined, type: 'auto' | 'manual') => any;
   selected?: NftCollection;
   filters?: CollectionFilterModel[] | boolean | 'auto';
   selectedManual?: CollectionFilterModel;
-  shopId?: string;
-  showAllFilters: boolean;
   search?: boolean;
   candyShopAddress?: string;
+  filterType?: FilterType;
 }
 
 const Logger = 'CandyShopUI/Collection';
@@ -28,22 +32,15 @@ export const CollectionFilter: React.FC<CollectionFilterProps> = ({
   selected,
   filters,
   selectedManual,
-  shopId,
-  showAllFilters,
   search,
-  candyShopAddress
+  candyShopAddress,
+  filterType
 }) => {
   const [options, setOptions] = useState<NftCollection[]>([]);
   const [offset, setOffset] = useState<number>(0);
   const [loading, setLoading] = useState<LoadStatus>(LoadStatus.ToLoad);
   const [haveNext, setHaveNext] = useState<boolean>(true);
   const [keyword, setKeyword] = useState<string>();
-  const [previousShopId, setPreviousShopId] = useState<string | undefined>(shopId);
-
-  if (shopId !== previousShopId) {
-    setPreviousShopId(shopId);
-    setOffset(0);
-  }
 
   const onSearch = useCallback((keyword: string) => {
     setKeyword(keyword);
@@ -51,24 +48,23 @@ export const CollectionFilter: React.FC<CollectionFilterProps> = ({
   }, []);
 
   const getFetchCollectionAPI = useCallback(
-    (startIndex: number) => {
+    (startIndex: number, candyShopAddress: string) => {
       const queryDto = {
         offset: startIndex,
         limit: LIMIT,
-        shopId: shopId || candyShopAddress,
+        shopId: candyShopAddress,
         name: keyword
       };
-      if (shopId) return fetchCollectionByShopAddress(queryDto);
-      return fetchAllCollection(queryDto);
+      return fetchCollectionByShopAddress(queryDto);
     },
-    [candyShopAddress, keyword, shopId]
+    [keyword]
   );
 
   const fetchOption = useCallback(
     (startIndex: number) => {
       if (!candyShopAddress) return;
       setLoading(LoadStatus.Loading);
-      getFetchCollectionAPI(startIndex)
+      getFetchCollectionAPI(startIndex, candyShopAddress)
         .then((res: ListBase<NftCollection>) => {
           const { result, offset, totalCount, count } = res;
           setHaveNext(offset + count < totalCount);
@@ -106,24 +102,70 @@ export const CollectionFilter: React.FC<CollectionFilterProps> = ({
     });
   }, [filters, keyword]);
 
+  const onTogglePopover = (open: boolean) => {
+    if (open === false) {
+      setOffset(0);
+      setKeyword('');
+    }
+  };
+
+  const disableLoadMore = !haveNext || loading === LoadStatus.Loading;
+
   // Manual filter
   if (Array.isArray(filters)) {
+    // Manual filter with dropdown type
+    if (filterType === 'dropdown') {
+      return (
+        <div>
+          <Popover.Root onOpenChange={onTogglePopover}>
+            <Popover.Trigger asChild>
+              <div className="candy-filter-collection-trigger">
+                <IconFilter /> Collections
+              </div>
+            </Popover.Trigger>
+            <Popover.Content className="candy-filter-collection-content" sideOffset={5}>
+              <Search onSearch={onSearch} placeholder="Search collections" />
+              <ul className="candy-filter-options">
+                {filters.map((item) => (
+                  <Popover.Close key={item.name}>
+                    <li
+                      onClick={onChange(item, 'manual')}
+                      className={selectedManual?.name === item.name ? 'selected' : ''}
+                      title={item.name}
+                    >
+                      {item.name}
+                    </li>
+                  </Popover.Close>
+                ))}
+              </ul>
+
+              <Popover.Arrow className="PopoverArrow" />
+            </Popover.Content>
+          </Popover.Root>
+          <Show when={Boolean(selectedManual)}>
+            <div className="candy-filter-selected-name">
+              {selectedManual?.name}
+              <span className="close-icon" onClick={onChange(undefined, 'manual')} />
+            </div>
+          </Show>
+        </div>
+      );
+    }
+
+    // Manual filter with list type
     return (
       <>
         <div className="candy-filter-subtitle">Collections</div>
+        <Show when={search}>
+          <Search onSearch={onSearch} placeholder="Search collections" />
+        </Show>
+        <Show when={Boolean(selectedManual)}>
+          <div className="candy-filter-selected-name">
+            {selectedManual?.name}
+            <span className="close-icon" onClick={onChange(undefined, 'manual')} />
+          </div>
+        </Show>
         <ul>
-          {search && <Search onSearch={onSearch} placeholder="Search collections" />}
-          {selectedManual ? (
-            <div className="candy-filter-selected-name">
-              {selectedManual.name}
-              <span className="close-icon" onClick={onChange(undefined, 'manual')} />
-            </div>
-          ) : null}
-          {!showAllFilters && (
-            <li key="All" onClick={onChange(undefined, 'manual')} className={selectedManual ? '' : 'selected'}>
-              All
-            </li>
-          )}
           {filteredList.map((filter) => {
             return (
               <li
@@ -140,34 +182,83 @@ export const CollectionFilter: React.FC<CollectionFilterProps> = ({
     );
   }
 
-  const disableLoadMore = !haveNext || loading === LoadStatus.Loading;
+  // Auto view
+  // Auto filter with dropdown type
+  if (filterType === 'dropdown') {
+    return (
+      <div>
+        <Popover.Root onOpenChange={onTogglePopover}>
+          <Popover.Trigger asChild>
+            <div className="candy-filter-collection-trigger">
+              <IconFilter /> Collections
+            </div>
+          </Popover.Trigger>
+          <Popover.Content className="candy-filter-collection-content" sideOffset={5}>
+            <Search onSearch={onSearch} placeholder="Search collections" />
+            <ul className="candy-filter-options">
+              {options.map((item) => (
+                <Popover.Close key={item.id}>
+                  <li onClick={onChange(item, 'auto')} className={selected?.id === item.id ? 'selected' : ''}>
+                    {item.name}
+                  </li>
+                </Popover.Close>
+              ))}
+            </ul>
+            <Show when={loading === LoadStatus.Loading}>
+              <Processing />
+            </Show>
+            <Show when={!disableLoadMore}>
+              <button className="candy-filter-load" type="button" onClick={() => fetchOption(offset)}>
+                + Load more
+              </button>
+            </Show>
 
+            <Popover.Arrow className="PopoverArrow" />
+          </Popover.Content>
+        </Popover.Root>
+        <Show when={Boolean(selected)}>
+          <div className="candy-filter-selected-name">
+            {selected?.name}
+            <span className="close-icon" onClick={onChange(undefined, 'auto')} />
+          </div>
+        </Show>
+      </div>
+    );
+  }
+
+  // Auto filter with list type
   return (
     <div className="candy-collection-filter">
       <div className="candy-filter-subtitle">Collections</div>
-      {search && <Search onSearch={onSearch} placeholder="Search collections" />}
-      {selected ? (
+      <Show when={search}>
+        <Search onSearch={onSearch} placeholder="Search collections" />
+      </Show>
+      <Show when={Boolean(selected)}>
         <div className="candy-filter-selected-name">
-          {selected.name}
+          {selected?.name}
           <span className="close-icon" onClick={onChange(undefined, 'auto')} />
         </div>
-      ) : null}
+      </Show>
+
       <ul className="candy-filter-options">
         {options.map((item) => (
           <li key={item.id} onClick={onChange(item, 'auto')} className={selected === item ? 'selected' : ''}>
             {item.name}
           </li>
         ))}
-        {loading === LoadStatus.Loading && <Processing />}
-        {!disableLoadMore && (
-          <button
-            className={`candy-filter-load ${disableLoadMore ? 'candy-filter-load-disable' : ''}`}
-            onClick={() => fetchOption(offset)}
-          >
-            + Load more
-          </button>
-        )}
       </ul>
+      <Show when={loading === LoadStatus.Loading}>
+        <Processing />
+      </Show>
+
+      <Show when={!disableLoadMore}>
+        <button
+          className={`candy-filter-load ${disableLoadMore ? 'candy-filter-load-disable' : ''}`}
+          onClick={() => fetchOption(offset)}
+        >
+          + Load more
+        </button>
+      </Show>
     </div>
   );
 };
